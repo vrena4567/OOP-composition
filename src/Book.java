cdd85c9c16ed1945a588629cb1db0aa8922a357a
vrena4567 <v.rena4567@gmail.com>
2023-06-12 16:32:20 +0200

    add and test book

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,65 @@
-public class Book {
+public class Book{
+    private String name;
+    private Author author;
+    private double price;
+    private int qty = 0;
+
+    public Book(String name, Author author, double price) {
+        this.name = name;
+        this.author = author;
+        this.price = price;
+    }
+
+    public Book(String name, Author author, double price, int qty) {
+        this.name = name;
+        this.author = author;
+        this.price = price;
+        this.qty = qty;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public String toString() {
+        return "Book[name= " + name + author.toString() + ", price= " + price + ", qty= " + qty + "]";
+    }
+
+    public String getAuthorName() {
+        return author.getName();
+    }
+
+    public String getAuthorEmail() {
+        return author.getEmail();
+    }
+
+    public String getAuthorGender() {
+        if (author.getGender() == 'f') {
+            return "female";
+        }
+        else if (author.getGender() == 'm') {
+            return "male";
+        }
+        return "gender letter falls out of preset character options";
+    }
 }
--- src/TestBook.java
+++ src/TestBook.java
@@ -1,2 +1,34 @@
 public class TestBook {
+    public static void main(String[] args) {
+        // Construct an author instance
+        Author ahTeck = new Author("Tan Ah Teck", "ahteck@nowhere.com", 'm');
+        System.out.println(ahTeck);  // Author's toString()
+
+        Book dummyBook = new Book("Java for dummy", ahTeck, 19.95, 99);  // Test Book's Constructor
+        System.out.println(dummyBook);  // Test Book's toString()
+
+// Test Getters and Setters
+        dummyBook.setPrice(29.95);
+        dummyBook.setQty(28);
+        System.out.println("name is: " + dummyBook.getName());
+        System.out.println("price is: " + dummyBook.getPrice());
+        System.out.println("qty is: " + dummyBook.getQty());
+        System.out.println("Author is: " + dummyBook.getAuthor());  // Author's toString()
+        System.out.println("Author's name is: " + dummyBook.getAuthor().getName());
+        System.out.println("Author's email is: " + dummyBook.getAuthor().getEmail());
+
+// Use an anonymous instance of Author to construct a Book instance
+        Book anotherBook = new Book("more Java",
+                new Author("Paul Tan", "paul@somewhere.com", 'm'), 29.95);
+        System.out.println(anotherBook);  // toString()
+
+// Printing the name and email of the author from a Book instance.
+        System.out.println("Name of the author of the 'Java for dummy' book: " + dummyBook.getAuthor().getName());
+        System.out.println("Email of the author of the same book: " + dummyBook.getAuthor().getEmail());
+
+// The same but with methods from the Book class.
+        System.out.println("Name of the author of the 'more Java' book: " + anotherBook.getAuthorName());
+        System.out.println("Email of the author of the same book: " + anotherBook.getAuthorEmail());
+        System.out.println("Gender of the author: " + anotherBook.getAuthorGender());
+    }
 }
